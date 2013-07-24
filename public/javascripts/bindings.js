7ca8d2e016ca7e3cc5a5d9ea2e3a27c9f0d2caa3
Danesh Harjani <danesh@dnharjani.com>
2013-07-24 13:27:54 +0100

    added top calendar with links to events

--- public/javascripts/bindings.js
+++ public/javascripts/bindings.js
@@ -2,14 +2,17 @@ var AppModel = function(){
     var self = this;
     self.redditPosts = ko.observableArray();
     self.showError = ko.observable(false);
+    self.dateList = ko.observableArray();
+    var monthNamesShort = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
 
     self.initialize = function(){
         if(navigator.onLine){
             $.getJSON('/posts')
                 .done(function(data){
                     localStorage.setItem("lsc-events", JSON.stringify(data));
-
                     setupPostsArray(data);
+                    setupDatesList(data);
                 })
                 .fail(function(){
                     // Error message
@@ -20,6 +23,7 @@ var AppModel = function(){
              if(events !== undefined && events !== null){
                  var parsedEvents = JSON.parse(events);
                  setupPostsArray(parsedEvents);
+                 setupDatesList(parsedEvents);
              }
             else{
                  // Error message
@@ -67,6 +71,15 @@ var AppModel = function(){
         return (index() % 2 === 0 ? 'post-container even' : 'post-container odd')
     };
 
+    self.showEventsOnDay = function(item, event){
+        if(item.events.length > 0){
+            $(document.body).animate({
+                'scrollTop':   $('#'+item.events[0].id).offset().top
+            }, 400);
+        }
+
+    };
+
     var setupPostsArray = function(redditPostData){
         _.each(redditPostData, function(item){
             item.data.showSummary = ko.observable(false);
@@ -74,6 +87,25 @@ var AppModel = function(){
             item.data.insLoaded = false;
             self.redditPosts.push(item.data);
         });
+    };
+
+    var setupDatesList = function(redditPostData){
+        var currentDate =  new Date().setHours(0,0,0,0);
+
+        for(var i = 0; i<60 ; i++){
+            var dayObject = {};
+            var calendarDate = currentDate + (i*86400000);
+            dayObject.day =  new Date(calendarDate).getDate();
+            dayObject.month =  monthNamesShort[new Date(calendarDate).getMonth()];
+            dayObject.year =  new Date(calendarDate).getFullYear();
+            dayObject.events = [];
+            _.each(redditPostData, function(item){
+                if(Date.parse(item.data.scheduledDate) === calendarDate){
+                    dayObject.events.push(item.data);
+                }
+            });
+            self.dateList.push(dayObject);
+        }
     }
 
 };
